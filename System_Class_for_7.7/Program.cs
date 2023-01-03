e77708b319b14ba2bda0a487939bb048a70237bc
Manson <Manson@MANSON>
2023-01-03 21:59:29 +0300

    Добавьте файлы проекта.

---
+++ System_Class_for_7.7/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace System_Class_for_7._7
+{
+
+    class User
+    {
+        public string Name;
+        public string LastName;
+    }
+
+    abstract class Delivery
+    {
+        public string Address;
+    }
+
+    class HomeDelivery : Delivery
+    {
+
+    }
+
+    class PickPointDelivery : Delivery
+    {
+
+    }
+
+    class Order<TDelivery> where TDelivery : Delivery
+    {
+        public TDelivery Delivery;
+        public int Number;
+        public string Description;
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+
+        Product[] Products { get; set; }
+    }
+
+    abstract class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+
+    class Food : Product
+    {
+        public string Type { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    class Vegetables : Food
+    {
+
+    }
+
+    class Fruits : Food
+    {
+
+    }
+
+    class Seafood : Food
+    {
+
+    }
+
+    class Phone : Product
+    {
+        public string Brand;
+        public string Model;
+        public string Color;
+    }
+}
